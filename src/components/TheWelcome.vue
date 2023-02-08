af40412467d44056261df9251db761d525228678
BeneRichi <benedict.ritschel@cec.valantic.com>
2023-02-08 22:05:50 +0100

    Creates a new Vue.js 3 project using the Vite build tool

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hero">
+    <h1>Start</h1>
+  </div>
+</template>
+
+
+<style lang="scss">
+.hero {
+  @apply lg:min-h-screen lg:flex lg:items-center;
+
+  h1 {
+    @apply text-xl font-medium text-white;
+  }
+}
+</style>
